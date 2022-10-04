0a649b065ef4d033042725a68b02f0b88ddece80
Bianca Milea <alumil2013@ieselcaminas.org>
2022-10-04 21:53:42 +0200

    example 5

--- src/Example5.java
+++ src/Example5.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Example5 {
+import java.util.Scanner;
+class Example5 {
+    public static void main(String[] argv) {
+
+        double r;
+        Scanner scanner = new Scanner(System.in);
+        r = scanner.nextDouble();
+
+        double largo = (2 * 3.14) * r;
+        double area = (3.14 * r) * r;
+
+        System.out.println(largo);
+        System.out.println(area);
+    }
 }
